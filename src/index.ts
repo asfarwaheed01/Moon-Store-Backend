5077065a094a77780b9c70b79769c4ff4e0bfd87
asfarwaheed01 <asfarwaheedch123@gmail.com>
2024-05-16 15:13:12 +0500

    socket implementation

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,8 @@ import userRoutes from "./routes/userRoutes";
 import stripeRoutes from "./routes/StripeRoutes";
 import orderRoutes from "./routes/OrderRoutes";
 import connectToDatabase from "./db/db";
+import { Server } from "socket.io";
+import { createServer } from "http";
 
 const app: Application = express();
 app.use(express.json());
@@ -15,6 +17,15 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(cors());
 dotenv.config();
+
+const server = createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+    credentials: true,
+  },
+});
 // const PORT: number = 8000 || process.env.PORT;
 const PORT: number = Number(process.env.PORT);
 console.log(process.env.PORT);
@@ -28,6 +39,20 @@ app.use("/api/users", userRoutes);
 app.use("/stripe", stripeRoutes);
 app.use("/order", orderRoutes);
 
-app.listen(PORT, () => {
+io.on("connection", (socket) => {
+  console.log("a user connected");
+  socket.on("message", (msg) => {
+    console.log("message: " + msg);
+    socket.broadcast.emit("message", msg);
+  });
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+});
+
+// app.listen(PORT, () => {
+//   console.log(`Server is running on PORT: ${PORT}`);
+// });
+server.listen(PORT, () => {
   console.log(`Server is running on PORT: ${PORT}`);
 });
